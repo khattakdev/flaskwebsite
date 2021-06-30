a8b0e917e80855f29701f8fe4edebe739ffe56b6
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-06-30 17:37:06 -0400

    sqlalchemy branch

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,37 @@ from dotenv import load_dotenv
 import smtplib
 from email.mime.text import MIMEText
 from werkzeug.security import check_password_hash, generate_password_hash
-from . import db
-from app.db import get_db
+# from . import db
+# from app.db import get_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
+class UserModel(db.Model):
+    __tablename__ = 'users'
 
-load_dotenv()
-app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 
 @app.route('/')
