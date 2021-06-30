671b82e2b520f56b51661eac1c124caa8bac57b3
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-06-29 23:51:49 -0400

    latest vr for docker

--- app/__init__.py
+++ app/__init__.py
@@ -3,35 +3,47 @@ from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
 import smtplib
 from email.mime.text import MIMEText
+from werkzeug.security import check_password_hash, generate_password_hash
+from . import db
+from app.db import get_db
+
+app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
+
+
 
 load_dotenv()
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
+
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
-    
+
 @app.route('/about')
 def about():
-    return render_template('about.html', title="MLH Fellow", url=os.getenv("URL"))   
+    return render_template('about.html', title="MLH Fellow", url=os.getenv("URL"))
 
 @app.route('/portfolio')
-def portfolio(): 
+def portfolio():
     return render_template('portfolio.html', title="Portfolio", url=os.getenv("URL"))
 
 @app.route('/resume')
-def resume(): 
+def resume():
     return render_template('resume.html', title="Resume", url=os.getenv("URL"))
 
 @app.route('/contact')
-def contact(): 
+def contact():
     return render_template('contact.html', title="Contact", url=os.getenv("URL"))
 
 @app.route('/send-email', methods=['GET','POST'])
 def send_email():
     response="Your message was sent succesfully!"
-    
+
     try:
         # HTTP POST Request args
         email_sender = request.form['email']
@@ -58,6 +70,65 @@ def send_email():
         server.quit()
     except:
         response="Sorry, there was an error."
-    
+
     return render_template('contact.html', title="Contact", response=response, url=os.getenv("URL"))
 
+
+@app.route('/health')
+def health():
+    return render_template('health.html', title="Health", url=os.getenv("URL"))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+    # ## TODO: Return a restister page
+    # return "Register Page not yet implemented", 501
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    # ## TODO: Return a login page
+    # return "Login Page not yet implemented", 501
+    return render_template('login.html', title="Login", url=os.getenv("URL"))
